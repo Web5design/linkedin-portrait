c571cc552e22ae3d0aaaf2512b8990de86b3420a
Anant Bhardwaj <anantb@csail.mit.edu>
2013-07-12 21:47:03 -0700

    auth

--- portrait/urls.py
+++ portrait/urls.py
@@ -6,12 +6,8 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'portrait.views.home', name='home'),
-    # url(r'^portrait/', include('portrait.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'portrait.views.home', name='home'),
+    url(r'^home', 'portrait.views.home', name='home'),
+    url(r'^auth', 'portrait.views.auth', name='auth'),
+    url(r'^auth_verify', 'portrait.views.auth_verify', name='auth_verify'),
 )
